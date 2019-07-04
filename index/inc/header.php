175738a57b6bf43c47b7badd423bc52cb90ff5e0
Vitor Hugo <vitor1213.dev@gmail.com>
2019-07-04 19:43:44 -0300

    atualização do header e novo arquivo atividades

--- index/inc/header.php
+++ index/inc/header.php
@@ -11,6 +11,12 @@
 	<link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/jumbotron/">
 
 	<style>
+	   * {
+		margin:0px;
+		pading:0px;
+		font-family: sans-serif;
+	   }
+	
       .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
@@ -30,8 +36,8 @@
 </head>
 <body>
 
-	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-	  <a class="navbar-brand" href="#">Navbar</a>
+	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
+	  <a class="navbar-brand" href="#">Aula 6</a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	  </button>
@@ -39,20 +45,20 @@
 	  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
 		<ul class="navbar-nav mr-auto">
 		  <li class="nav-item active">
-			<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
 		  </li>
 		  <li class="nav-item">
-			<a class="nav-link" href="#">Link</a>
+			<a class="nav-link" href="atividades.php">Atividades</a>
 		  </li>
 		  <li class="nav-item">
-			<a class="nav-link disabled" href="#">Disabled</a>
+			<a class="nav-link" href="#">Informações</a>
 		  </li>
 		  <li class="nav-item dropdown">
-			<a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+			<a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Mais</a>
 			<div class="dropdown-menu" aria-labelledby="dropdown01">
-			  <a class="dropdown-item" href="#">Action</a>
-			  <a class="dropdown-item" href="#">Another action</a>
-			  <a class="dropdown-item" href="#">Something else here</a>
+			  <a class="dropdown-item" href="#">Detalhes</a>
+			  <a class="dropdown-item" href="#">Fotos</a>
+			  <a class="dropdown-item" href="#">Privacidade</a>
 			</div>
 		  </li>
 		</ul>
