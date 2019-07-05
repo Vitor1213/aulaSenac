95bcaf6b7a731fbf342f17f3d44c8927243c5d21
Vitor Hugo <vitor1213.dev@gmail.com>
2019-07-04 21:09:35 -0300

    ../

--- index/index.php
+++ index/index.php
@@ -6,7 +6,7 @@
 	<div class="jumbotron">
 		<div class="container">
 		  <h1 class="display-3">Laranja!</h1>
-		  <p>Massagistas Ruivas Pintadas RJ.</p>
+		  <p>Massagistas Loiras.</p>
 
 		  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
 		</div>
